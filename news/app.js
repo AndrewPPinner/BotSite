0150c2ce17543646d7185fbc40c4d51bd7632142
AndrewPPinner <andppinner@gmail.com>
2022-03-18 11:45:42 -0400

    Updating all endpoints

--- news/app.js
+++ news/app.js
@@ -11,7 +11,7 @@ const app = Vue.createApp({
         //get token for authorization to use endpoint
         getToken(token) {
             axios
-            .get("https://gpubots.asuscomm.com/profile/ID")
+            .get("https://andrew-pinner.asuscomm.com/botsite/profile/ID")
             .then(response => {
                 const token = response.data
                 this.getStock(token)
@@ -22,7 +22,7 @@ const app = Vue.createApp({
 //get stock information from server
         getStock(token) {
             axios({method: 'get',
-            url: 'https://gpubots.asuscomm.com/stock',
+            url: 'https://andrew-pinner.asuscomm.com/botsite/stock',
             headers: { "authorization": "Bearer " + token}})
             .then(res => (this.response = res.data, console.log(res)))
             .catch(e => (console.log(e)))
@@ -39,14 +39,14 @@ const app = Vue.createApp({
     mounted() {
         //check if logged in
         axios
-        .get("https://gpubots.asuscomm.com/loggedin")
+        .get("https://andrew-pinner.asuscomm.com/botsite/loggedin")
         .then(res => (this.loggedIn = true))
         .catch(e => (this.loggedIn = false))
 
         //get profile picture
         axios
         ({method: 'get',
-        url: 'https://gpubots.asuscomm.com/profile/info'})
+        url: 'https://andrew-pinner.asuscomm.com/botsite/profile/info'})
         .then(response => {
         this.profilePicture = "https://avatars.dicebear.com/api/bottts/" + response.data.nickname + ".svg"})
         .catch(e => (console.log(e)))
